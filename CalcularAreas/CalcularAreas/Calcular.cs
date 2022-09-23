ede95d56a003bf1669033e8e1f61191594e5fd20
Daniel Alejandro Alegria Carrasco <alexodaac@hotmail.com>
2022-09-23 01:16:57 -0500

    Servicios web agregados

---
+++ CalcularAreas/CalcularAreas/Calcular.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalcularAreas
+{
+    public class Calcular
+    {
+        public decimal N1 { get; set; }
+        public decimal N2 { get; set; }
+        
+        public decimal AreaCuadrado()
+        {
+            return N1 * N1;
+        }
+
+        public decimal AreaTriangulo()
+        {
+            return N1 * N2 / 2; 
+        }
+
+        public decimal AreaCirculo()
+        {
+            decimal Pi = 3.141592M;
+            return Pi * (N1 * N1);
+        }
+    }
+}
--- CalcularAreas/CalcularAreas/CalcularWebService.asmx.cs
+++ CalcularAreas/CalcularAreas/CalcularWebService.asmx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.Services;
 
@@ -21,6 +22,47 @@ namespace CalcularAreas
         public string HelloWorld()
         {
             return "Hola a todos";
+        }
+
+        /// <summary>
+        /// Método que solicita la logitud del lado de un cuadrado y devuelve su área
+        /// </summary>
+        /// <param name="Lado">Decimal</param>
+        /// <returns>Área del cuadrado</returns>
+        [WebMethod(Description ="Método que devuelve el área de un cuadrado con base en la longitud de un lado.")]
+        public decimal AreaCuadrado(decimal Lado)
+        {
+            Calcular c = new Calcular();
+            c.N1 = Lado;
+            return c.AreaCuadrado();
+        }
+
+        /// <summary>
+        /// Método que solicita la base y la altura de un triángulo
+        /// </summary>
+        /// <param name="Base">Decimal</param>
+        /// <param name="Altura">Decimal</param>
+        /// <returns>Área del triángulo</returns>
+        [WebMethod(Description ="Método que devuelve el área de un triángulo con la longitud de la base y la altura.")]
+        public decimal AreaTriangulo(decimal Base, decimal Altura)
+        {
+            Calcular c = new Calcular();
+            c.N1 = Base;
+            c.N2 = Altura;
+            return c.AreaTriangulo();
+        }
+
+        /// <summary>
+        /// Método que solicita el radio de un círculo
+        /// </summary>
+        /// <param name="Radio">Decimal</param>
+        /// <returns>Área del círculo</returns>
+        [WebMethod(Description ="Método que devuelve el área de un círculo con base en el radio.")]
+        public decimal AreaCirculo(decimal Radio)
+        {
+            Calcular c = new Calcular();
+            c.N1 = Radio;
+            return c.AreaCirculo();
         }
     }
 }
